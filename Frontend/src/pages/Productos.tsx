493946c02d4e799fb73db65438fd30fab78f8d73
Mauricio <mauricioherraz66@gmail.com>
2026-06-16 23:07:05 -0400

    feat: se añade ConsultorVet, Dashboard, MapaCompetitivo y Productos en sus respectivas carpetas ya arreglo de back y front

--- Frontend/src/pages/Productos.tsx
+++ Frontend/src/pages/Productos.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getProducts } from "./services/api";
+import { getProducts } from "../services/api";
 
 type Props = {
   language?: "es" | "en";
